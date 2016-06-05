17ac7602f858df9cf610540591717857106b3780
Pritam Sarkar <001pritam2012@gmail.com>
2016-06-05 16:34:14 +0530

    Iron pawn animated appearance

--- Assets/Scripts/pawn_generator.cs
+++ Assets/Scripts/pawn_generator.cs
@@ -14,9 +14,20 @@ public class pawn_generator : MonoBehaviour {
 	void Awake () {
 		Resources.UnloadUnusedAssets();
 		for(int i = 0; i < 4; i++){
-			Instantiate(IronPawn, new Vector3(4.5f, 1.11f, -0.5f + (float)i), Quaternion.identity);
-			GameObject tempObj = Instantiate(IronPawn, new Vector3(1.5f, 1.11f, -0.5f + (float)i), Quaternion.identity) as GameObject;
-			tempObj.GetComponent<Renderer>().material = blackMaterial;
+			GameObject tempObj1 = Instantiate(IronPawn, new Vector3(4.5f, 0.5f, -0.5f + (float)i), Quaternion.identity) as GameObject;
+			StartCoroutine(MoveFromTo(tempObj1, new Vector3(4.5f, 0.5f, -0.5f + (float)i), new Vector3(4.5f, 1.11f, -0.5f + (float)i), 3f));
+			GameObject tempObj2 = Instantiate(IronPawn, new Vector3(1.5f, 1.11f, -0.5f + (float)i), Quaternion.identity) as GameObject;
+			tempObj2.GetComponent<Renderer>().material = blackMaterial;
+			StartCoroutine(MoveFromTo(tempObj2, new Vector3(1.5f, 0.5f, -0.5f + (float)i), new Vector3(1.5f, 1.11f, -0.5f + (float)i), 3f));
+		}
+	}
+
+	IEnumerator MoveFromTo(GameObject gameObject, Vector3 vectorFrom, Vector3 vectorTo, float time){
+		float t = 0f;
+		while (t < time) {
+			t += Time.deltaTime; // Sweeps from 0 to time in seconds
+			gameObject.transform.position = Vector3.Lerp(vectorFrom, vectorTo, t);
+			yield return null;         // Leave the routine and return here in the next frame
 		}
 	}
 	
