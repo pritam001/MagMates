5923abc84bb342afc3c631b286f48da018d6d6d8
Pritam Sarkar <001pritam2012@gmail.com>
2016-06-30 14:40:52 +0530

    Add new board

--- Assets/Scripts/movement_controller.cs
+++ Assets/Scripts/movement_controller.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
+using System.IO;
+using System.Collections.Generic;
 
 public class movement_controller : MonoBehaviour {
 
@@ -36,6 +40,7 @@ public class movement_controller : MonoBehaviour {
 		}
 		if (Input.GetButtonDown("Fire2")) {
 			glowOff();
+			StartCoroutine(moveAnimStep(GameObject.Find("MagnetPawnA"), new Vector3(0,0,1), 1f));
 		}
 	}
 
@@ -48,6 +53,16 @@ public class movement_controller : MonoBehaviour {
 		for(int i = 1; i <= 24; i++){
 			GameObject go = GameObject.Find("Cube" + i + " (1)");
 			go.GetComponent<Renderer>().material = glass_mat;
+		}
+	}
+
+	IEnumerator moveAnimStep(GameObject gameObject, Vector3 v, float time){
+		float t = 0f;
+		Vector3 vectorStart = gameObject.transform.position;
+		while (t < time) {
+			t += Time.deltaTime; // Sweeps from 0 to time in seconds
+			gameObject.transform.position = Vector3.Lerp(vectorStart, vectorStart + v, t);
+			yield return null;         // Leave the routine and return here in the next frame
 		}
 	}
 }
