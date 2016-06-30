7bffb45f854a690734c2cbad14f594ccdfb51a8e
Pritam Sarkar <001pritam2012@gmail.com>
2016-06-30 19:33:54 +0530

    Add 6 Iron pawns

--- Assets/Scripts/pawn_generator.cs
+++ Assets/Scripts/pawn_generator.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Collections;
@@ -13,12 +13,12 @@ public class pawn_generator : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		Resources.UnloadUnusedAssets();
-		for(int i = 0; i < 4; i++){
-			GameObject tempObj1 = Instantiate(IronPawn, new Vector3(4.5f, 0.5f, -0.5f + (float)i), Quaternion.identity) as GameObject;
-			StartCoroutine(MoveFromTo(tempObj1, new Vector3(4.5f, 0.5f, -0.5f + (float)i), new Vector3(4.5f, 1.12f, -0.5f + (float)i), 3f));
+		for(int i = 0; i < 6; i++){
+			GameObject tempObj1 = Instantiate(IronPawn, new Vector3(4.5f, 0.5f, -1.5f + (float)i), Quaternion.identity) as GameObject;
+			StartCoroutine(MoveFromTo(tempObj1, new Vector3(4.5f, 0.5f, -1.5f + (float)i), new Vector3(4.5f, 1.12f, -1.5f + (float)i), 3f));
 			GameObject tempObj2 = Instantiate(IronPawn, new Vector3(1.5f, 0.5f, -0.5f + (float)i), Quaternion.identity) as GameObject;
 			tempObj2.GetComponent<Renderer>().material = blackMaterial;
-			StartCoroutine(MoveFromTo(tempObj2, new Vector3(1.5f, 0.5f, -0.5f + (float)i), new Vector3(1.5f, 1.12f, -0.5f + (float)i), 3f));
+			StartCoroutine(MoveFromTo(tempObj2, new Vector3(1.5f, 0.5f, -1.5f + (float)i), new Vector3(1.5f, 1.12f, -1.5f + (float)i), 3f));
 		}
 	}
 
@@ -33,6 +33,6 @@ public class pawn_generator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
