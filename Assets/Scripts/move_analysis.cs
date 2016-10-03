9fe6e53a8b3e8283a49da404e2481f880689dfc2
Pritam Sarkar <001pritam2012@gmail.com>
2016-10-03 20:58:09 +0530

    learning curve analysis via movement log

---
+++ Assets/Scripts/move_analysis.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_analysis : MonoBehaviour {
+	public static int strategy_point_p1 = 0;
+	public static int strategy_point_p2 = 0;
+	public static int learning_point_p1 = 0; 
+	public static int learning_point_p2 = 0; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void learning_modifier(string move,int playerNo){
+		switch (move) {
+		case "swap":
+			learning_modifier (10, playerNo);
+			break;
+		case "wrong_swap":
+			learning_modifier (-5, playerNo);
+			break;
+		case "plastic_swap":
+			learning_modifier (-20, playerNo);
+			break;
+		default:
+			Debug.Log ("Move not found for learning_modifier (" + move + "," + playerNo + ")");
+			break;
+		}
+		
+	}
+
+	public static void learning_modifier(int num,int playerNo){
+		if (playerNo == 1) {
+			learning_point_p1 += num;
+		} else {
+			learning_point_p2 += num;
+		}
+	}
+}
